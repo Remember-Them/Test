43b836b4cf2f846066973db0c5d37265f70d18ee
Remember-Them <1439464054@qq.com>
2020-02-14 20:10:45 +0800

    个人简历

---
+++ js/First.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(  '.nav a').click(function(e){
+        var height = $(window).height();
+        var target = $(this).attr('index');
+        var finalH = 0;
+        finalH = height * target;
+
+        $('body,html').animate({ 
+            scrollTop: finalH
+        },500,function(){
+            e.preventDefault();
+            return false;
+        });
+    })
+})
+   
+
